75ed5367d1c3750a92cedfb08e797a17e316006a
chithra <chithra7123@gmail.com>
2025-11-20 21:30:02 +0000

    paginator and search feature along with product viewing till cart viewing done

--- store/urls.py
+++ store/urls.py
@@ -4,14 +4,13 @@ from store import views
 urlpatterns = [
 
     path('', views.store, name='store'),
-    path('product/', views.products, name='products'),  # html for product.html
-    path('cart/', views.cart, name='cart'),  # html for cart.html
+    path('search/', views.search, name="search"),
 
-    # html for checkout.html
     path('checkout/', views.checkout, name='checkout'),
 
     path('<slug:category_slug>/', views.store, name='products_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+    path('<slug:category_slug>/<slug:product_slug>/',
+         views.product_detail, name='product_detail'),
 
 
 
