09670d0f8374392c95f1ccb9cacfa0de20bcfeb2
chithra <chithra7123@gmail.com>
2026-01-08 16:08:40 +0000

    updated views.py for reviews defined problem

--- lcshop/views.py
+++ lcshop/views.py
@@ -1,15 +1,9 @@
-
-
-from django.http import HttpResponse
 from django.shortcuts import render
 from store.models import Product, ReviewRating
 
-
 def home(request):
-    products = Product.objects.all().filter(is_available=True).order_by('created_date')
-
-    for product in products:
-        reviews = ReviewRating.objects.filter(product_id = product.id, status=True)
+    products = Product.objects.filter(is_available=True).order_by('created_date')
+    reviews = ReviewRating.objects.filter(status=True)
 
     context = {
         'products': products,
