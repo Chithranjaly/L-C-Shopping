a25cc6e673c0322d5e4e9ceb44f861a414f59639
Chithra <chithra7123@gmail.com>
2026-06-12 22:46:54 +0100

    fix: export EB env vars before collectstatic to enable S3 upload

--- .platform/hooks/postdeploy/01_collectstatic.sh
+++ .platform/hooks/postdeploy/01_collectstatic.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+export $(cat /opt/elasticbeanstalk/deployment/env | grep -v '^#' | xargs)
 source /var/app/venv/*/bin/activate
 cd /var/app/current
 python manage.py migrate --noinput
